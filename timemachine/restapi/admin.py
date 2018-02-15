803d8731758b77daf713f9a6a6be762676b00cb0
berri3 <cmliang2004@hotmail.com>
2018-02-15 10:21:16 -0500

    Registered model in admin page for easier DB management

--- timemachine/restapi/admin.py
+++ timemachine/restapi/admin.py
@@ -1,3 +1,14 @@
 # from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import *
+
+# register stuff to show on admin page
+admin.site.register(Problem)
+admin.site.register(Rating)
+admin.site.register(Solution)
+admin.site.register(User)
+
